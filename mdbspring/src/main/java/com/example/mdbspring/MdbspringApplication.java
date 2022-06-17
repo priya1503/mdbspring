27f3f9f0af2767757ea866e04edc232a339e448d
priya1503 <blpriya1503@gmail.com>
2022-06-17 22:01:16 +0530

    First Commit

---
+++ mdbspring/src/main/java/com/example/mdbspring/MdbspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdbspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdbspringApplication.class, args);
+	}
+
+}
